cff463924cd3507537275883a7edb7a25f094d24
gabrieltaccola <gabriel.taccola.silva@escola.pr.gov.br>
2026-06-18 14:57:01 +0000

    ok

---
+++ jogo.py
@@ -0,0 +1,18 @@
+print("********************************")
+print("Bem vindo ao jogo de adivinhação")
+print("********************************")
+
+numeroseceto= 40
+
+chute = input("Digite o seu número: ")
+print("Você digitou: ", chute)
+
+chuteNúmerico = int(chute)
+
+#se você digitar qualquer número vou verificar se acertou ou errou
+if(numeroseceto == chuteNumerico):
+    print("Você acertou!")
+else:
+    print("Você errou!")
+
+print("Fim de jogo")
