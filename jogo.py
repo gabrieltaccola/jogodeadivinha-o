5209bc7f7821911613c8885893213e4c40120454
gabrieltaccola <gabriel.taccola.silva@escola.pr.gov.br>
2026-06-25 15:04:10 +0000

    ok

--- jogo.py
+++ jogo.py
@@ -2,7 +2,14 @@ print("********************************")
 print("Bem vindo ao jogo de adivinhação")
 print("********************************")
 
-numeroseceto= 40
+numeroseceto = random.randrange(1, 51)
+totaldetentativas = 10
+
+while(totaldetentativas > 0):
+    print("Você tem ", totaldetentativas, " tentativas")
+    totaldetentativas = totaldetentativas - 1
+    print("tentativa restante: ", totaldetentativas)
+    print("Qual é o seu chute?")
 
 chute = input("Digite o seu número: ")
 print("Você digitou: ", chute)
